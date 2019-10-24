88bde94f9efc98dd681734ce12c1f56b5a0c12ff
Ilya Bekirov <ilja.bekirov@gmail.com>
2019-10-25 00:05:49 +0300

    Implement articles#edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,18 +11,30 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(contact_params)
-    if @article.valid?
-      @article.save
+    @article = Article.new(article_params)
+    if @article.save
       redirect_to articles_path
     else
       render action: 'new' # new_contacts_path
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render action: 'edit' # new_contacts_path
+    end
+  end
+
   private
 
-  def contact_params
+  def article_params
     params.require(:article).permit(:title, :text)
   end
 end
