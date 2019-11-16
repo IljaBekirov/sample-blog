e567865952bcd0abcb9b0b3f5f3accb11bae7a42
Ilya Bekirov <ilja.bekirov@gmail.com>
2019-11-17 01:11:20 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,12 @@ describe Article do
   describe 'associations' do
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it 'returns the article title' do
+      article = create(:article, title: 'Foo Bag')
+
+      expect(article.subject).to eq('Foo Bag')
+    end
+  end
 end
