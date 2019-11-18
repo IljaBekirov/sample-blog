928e69bb1935d3614f796f6e09eac76b30204a11
Ilya Bekirov <ilja.bekirov@gmail.com>
2019-11-18 23:31:01 +0300

    Update tests (allows a guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -6,4 +6,14 @@ feature 'Contact creation' do
 
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario 'allows a guest to create contact' do
+    visit '/contacts'
+
+    fill_in :contact_email, with: 'user@mail.ru'
+    fill_in :contact_message, with: 'something'
+
+    click_button 'Отправить сообщение'
+    expect(page).to have_content 'Спасибо'
+  end
 end
